ac8337aebc835a11974d31d5082f598e5dd3be6d
Matthew LaForest <matt.laforest@rollbar.com>
2021-06-03 18:55:26 -0400

    demonstrate hook error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Provider, ErrorBoundary, useRollbarPerson } from "@rollbar/react"; // <-- Provider imports 'rollbar' for us
 
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+
+// same configuration you would create for the Rollbar.js SDK
+const rollbarConfig = {
+  accessToken: "POST_CLIENT_ITEM_ACCESS_TOKEN",
+  environment: "production",
+};
+
+async function getCurrentUser() {
+  return Promise.resolve({ name: "foobar" });
+}
+
+function SomeComponent() {
+  const [currentUser, setCurrentUser] = React.useState();
+
+  React.useEffect(() => {
+    async function getUser() {
+      const user = await getCurrentUser();
+      if (user) {
+        useRollbarPerson(user);
+      }
+      setCurrentUser(user);
+    }
+
+    getUser();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,10 +42,21 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ${currentUser ? currentUser.name : ""}
         </a>
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Provider config={rollbarConfig}>
+      {/* ErrorBoundary catches all React errors in the tree below and logs them to Rollbar */}
+      <ErrorBoundary>
+        <SomeComponent />
+      </ErrorBoundary>
+    </Provider>
   );
 }
 
